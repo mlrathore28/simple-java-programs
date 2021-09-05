36fce7577fd06404102b740691151e539905a573
Mohan Lal Rathore <90147813+mlrathore28@users.noreply.github.com>
2021-09-05 23:57:56 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,12 @@
+// Java Program to Print an Array
+
+public class Array {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int element: array) {
+            System.out.println(element);
+        }
+    }
+}
