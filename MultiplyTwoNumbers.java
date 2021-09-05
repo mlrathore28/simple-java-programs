159205aa1b4e4363487e3c23cbda06a8f0f75041
Mohan Lal Rathore <90147813+mlrathore28@users.noreply.github.com>
2021-09-05 23:34:28 +0530

    Create MultiplyTwoNumbers.java

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,15 @@
+// Java Program to Multiply two Floating Point Numbers
+
+// Header file for input output functions
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
