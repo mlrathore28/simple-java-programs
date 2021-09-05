9c7e38beb72c051ba0b8e10c69088573bc2538ec
Mohan Lal Rathore <90147813+mlrathore28@users.noreply.github.com>
2021-09-06 00:01:29 +0530

    Create CharString.java

---
+++ CharString.java
@@ -0,0 +1,13 @@
+// Java Program to Convert Character to String and Vice-Versa
+
+public class CharString {
+
+    public static void main(String[] args) {
+        char ch = 'c';
+        String st = Character.toString(ch);
+        // Alternatively
+        // st = String.valueOf(ch);
+
+        System.out.println("The string is: " + st);
+    }
+}
