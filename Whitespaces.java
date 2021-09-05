88dee5faf0405ebd0b1da4e63c08f9caccfaff9e
Mohan Lal Rathore <90147813+mlrathore28@users.noreply.github.com>
2021-09-05 23:57:17 +0530

    Create Whitespaces.java

---
+++ Whitespaces.java
@@ -0,0 +1,12 @@
+// Java Program to Remove All Whitespaces from a String
+
+public class Whitespaces {
+
+    public static void main(String[] args) {
+        String sentence = "T    his is b  ett     er.";
+        System.out.println("Original sentence: " + sentence);
+
+        sentence = sentence.replaceAll("\\s", "");
+        System.out.println("After replacement: " + sentence);
+    }
+}
