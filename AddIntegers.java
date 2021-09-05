d530877c5a183176e034adbc61ae66b221e20e3d
Mohan Lal Rathore <90147813+mlrathore28@users.noreply.github.com>
2021-09-05 23:33:25 +0530

    Create AddIntegers.java

---
+++ AddIntegers.java
@@ -0,0 +1,18 @@
+// Java Program to Add Two Integers
+
+// Header file for input output functions
+class AddIntegers {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 10;
+    int second = 20;
+    
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
+}
