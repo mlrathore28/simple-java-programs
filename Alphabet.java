a2d51266819b25a2c71aef3d30db09695eae7767
Mohan Lal Rathore <90147813+mlrathore28@users.noreply.github.com>
2021-09-05 23:42:04 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,14 @@
+// Java Program to Check Whether a Character is Alphabet or Not
+
+public class Alphabet {
+
+    public static void main(String[] args) {
+
+        char c = '*';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
